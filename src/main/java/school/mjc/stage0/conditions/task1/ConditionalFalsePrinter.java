c0381c31ff37220c4d0a997975898bc2ca50f56d
Daniel Oto <daniel.oto.mail@gmail.com>
2022-08-18 20:59:16 +0300

    module4-task1

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        int x = 3;
+        if (x > 5) {
+            System.out.println("false");
+        }
+        else {
+            System.out.println("false");
+        }
+
     }
 }
+
+
--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        if ("true".length() == 4) {
+            System.out.println("true");
+        }
     }
 }
